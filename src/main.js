452a3f4cb7ab6e4be02be4df3f4e0ea15e691e72
David Freitas <davidfreitas.dev@outlook.com>
2023-11-07 11:47:44 -0300

    commit inicial

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+
+import './services/firebase-firestore';
+import './assets/css/global.css';
+
+const pinia = createPinia();
+
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+
+app.mount('#app');
